4990427d99a99ff002351786c04ea38bc74c2f54
DemonDED <DemonDED@inbox.ru>
2026-03-25 09:59:44 +0700

    colorscheme for bashrc

---
+++ make_color.sh
@@ -0,0 +1,1 @@
+echo "export PS1="\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\[\e[0;32m\]\\$\[\e[m\] "" >> ~/.bashrc;source ~/.bashrc
